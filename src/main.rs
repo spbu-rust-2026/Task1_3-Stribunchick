d1fececc9633b1a4e1a3a5bcaeb26ebc6ef22643
Stribunchick <seregailin08@gmail.com>
2026-09-19 14:20:57 +0300

    Homework done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::process::exit;
+
+// Restrict access to root directory, handle unreadable directories
 fn main() {
-    println!("Hello, world!");
+    let mut path = String::new();
+    io::stdin().read_line(&mut path).expect("could not read");
+    let path = path.trim();
+    // check if the directory is readable
+    let check: bool = Path::new(&path).is_dir();
+    if check {
+        println!("failure");
+        exit(-1);
+    }
+    match fs::File::open(path) {
+        Ok(_s) => println!("success"),
+        Err(_e) => println!("failure"),
+    };
 }
