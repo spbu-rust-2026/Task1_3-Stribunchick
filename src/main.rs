7834819a99acdc7adfa6d9ccb6e7fa65395a83ac
Stribunchick <seregailin08@gmail.com>
2026-09-23 18:09:23 +0300

    Implemented the requested changes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use std::fs;
 use std::io;
 use std::path::Path;
-use std::process::exit;
 
 // Restrict access to root directory, handle unreadable directories
 fn main() {
     let mut path = String::new();
-    io::stdin().read_line(&mut path).expect("could not read");
+    match io::stdin().read_line(&mut path) {
+        Ok(_) => {}
+        Err(_e) => {
+            println!("failure");
+            return;
+        }
+    }
     let path = path.trim();
     // check if the directory is readable
     let check: bool = Path::new(&path).is_dir();
     if check {
         println!("failure");
-        exit(-1);
+        return;
     }
     match fs::File::open(path) {
         Ok(_s) => println!("success"),
